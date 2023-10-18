0988f738a46072d360c463b30324585ff4f49f44
Jonathan Mattingly <jmattingly2@gmail.com>
2023-10-17 18:06:00 -0700

    added cat question01

--- hw02script1.sh
+++ hw02script1.sh
@@ -1,11 +1,9 @@
-mkdir homework02
-
-
+	mkdir homework02
 	 echo My username is >> question01.txt
 	 echo $(whoami) >> question01.txt
 	 echo My home directory is >> question01.txt
 	 echo $(echo $HOME) >> question01.txt
 	 echo The contents of the tfcb_2023/lectures/lecture04 directory are >> question01.txt
-	 echo $(ls) >> question01.txt 
-	 
+	 echo $(ls) >> question01.txt  
 	 mv question01.txt homework02
+	 cat question01.txt
